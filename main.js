c9b7014e064e66e4b9097166c5b661693218fc66
Jeremy LEFEBVRE--AGNES <jeremy.la@me.com>
2017-12-22 12:22:03 +0100

    fix : package.json missing script to start

--- main.js
+++ main.js
@@ -2,19 +2,17 @@
 
 import express from 'express'
 import bodyParser from 'body-parser'
-if (process.env.NODE_ENV !== 'production'){
-	import logger from 'morgan'
-}
+import logger from 'morgan'
 
 import routes from './routes/index'
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json()
+app.use(bodyParser.json())
 app.disable('x-powered-by')
 if (process.env.NODE_ENV !== 'production'){
-	app.use(logger('dev')
+	app.use(logger('dev'))
 }
 app.use('/api', routes)
 app.listen(process.env.PORT || 8042)
